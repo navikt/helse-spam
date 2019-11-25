a2604545a6e581ef6ed9ed4c2c146f8694e6223c
royag-nav <roy.andre.gundersen@nav.no>
2019-11-25 14:07:20 +0100

    ha med utbetalingsreferanse

--- src/main/kotlin/no/nav/helse/Utbetalingsbehov.kt
+++ src/main/kotlin/no/nav/helse/Utbetalingsbehov.kt
@@ -1,7 +1,9 @@
 package no.nav.helse
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.apache.commons.codec.binary.Base32
 import java.math.BigDecimal
+import java.nio.ByteBuffer
 import java.time.LocalDate
 import java.util.UUID
 
@@ -9,12 +11,25 @@ data class Utbetalingsbehov(
     @JsonProperty("@behov")
     val behov: String,
     val sakskompleksId: UUID,
+    val utbetalingsreferanse: String = sakskompleksId.base32Encode(),
     val aktørId: String,
     val organisasjonsnummer: String,
     val maksdato: LocalDate,
     val saksbehandler: String,
     val utbetalingslinjer: List<Utbetalingslinje>
 )
+
+private fun UUID.base32Encode(): String {
+    val pad = '='
+    return Base32(pad.toByte())
+        .encodeAsString(this.byteArray())
+        .replace(pad.toString(), "")
+}
+
+private fun UUID.byteArray() = ByteBuffer.allocate(Long.SIZE_BYTES * 2).apply {
+    putLong(this@byteArray.mostSignificantBits)
+    putLong(this@byteArray.leastSignificantBits)
+}.array()
 
 data class Utbetalingslinje(
     val grad: Int,
